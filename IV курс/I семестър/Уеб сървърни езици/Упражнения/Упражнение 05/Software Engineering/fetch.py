2fc07908ad71d35dd65933fa87546abb5125e1f7
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2024-06-05 22:30:42 +0300

    Web Server Languages

---
+++ IV курс/I семестър/Уеб сървърни езици/Упражнения/Упражнение 05/Software Engineering/fetch.py
@@ -0,0 +1,39 @@
+
+import requests
+
+
+# url = "https://wizard-world-api.herokuapp.com/Houses"
+# response = requests.get(url)
+# print(response.json())S
+
+
+# response = requests.get(url)
+# GANJAlf = {}
+# for obj in response.json():
+#     name=obj.get('name')
+#     animal=obj.get('animal')
+#     GANJAlf[name]=animal
+
+#     print(name)
+
+# print(GANJAlf)
+# print(response.json())
+
+
+
+
+#url = "https://wizard-world-api.herokuapp.com/Spells"
+# params = {'name': 'Levitation Charm'}
+# params = {'incantation': 'Wingardium Leviosa'}
+#response = requests.get(url, params=params)
+#print(response.json())
+
+
+
+#how to pass headers to the request
+# url = 'https://api.github.com/some/endpoint'
+# headers = {'user-agent': 'my-app/0.0.1'}
+# response = requests.get(url, headers=headers, headers=headers)
+
+
+# https://wizard-world-api.herokuapp.com/swagger/index.html
