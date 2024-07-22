a88a293726b65f79536c4618a03a664adf1acb5e
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2024-07-22 21:49:12 +0300

    Computational Linguistics

---
+++ IV курс/II семестър/Компютърна лингвистика/Проекти/PLY Based Calculator/calclex.py
@@ -0,0 +1,58 @@
+# ------------------------------------------------------------
+# calclex.py
+#
+# tokenizer for a simple expression evaluator for
+# numbers and +,-,*,/
+# ------------------------------------------------------------
+import ply.lex as lex
+
+# List of token names.   This is always required
+tokens = (
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'FUNNAME',
+    'POW',
+    'COMMA'
+)
+# Regular expression rules for simple tokens
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_FUNNAME = r'[A-Z]+'
+t_POW = r'\^'
+t_COMMA = r','
+
+
+# A regular expression rule with some action code
+def t_NUMBER(t):
+    r'[\-+]?\d+(\.\d+)?'
+    t.value = float(t.value)
+    return t
+
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore = ' \t'
+
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+# Build the lexer
+lexer = lex.lex()
